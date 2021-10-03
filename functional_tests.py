c5807ede41669830ff8ccb33ea4e6a2328696e57
HeyItWorked <liamnguyenn98@gmail.com>
2021-10-03 15:37:41 -0500

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -43,7 +43,8 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             # The any() function returns True if any element of an iterable is True. If not, it returns False.
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
